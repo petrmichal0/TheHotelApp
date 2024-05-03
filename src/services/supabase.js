08ed4031ec338f2b65954f8516f1f2083e4e3d0a
Petr Michal <petrmichal0@gmail.com>
2024-05-04 01:02:33 +0200

    basic setting supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ssvvatskxyaexqasxfig.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzdnZhdHNreHlhZXhxYXN4ZmlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3NzE3NjcsImV4cCI6MjAzMDM0Nzc2N30.Ga5ybpQIgcaafCrQt_68sAor-lrKjigJUNsjDvDINI4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
